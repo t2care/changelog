bd7c4d8c35bbecb99a1c3b8e8404470c1d4f27c0
t2nt <106388825+t2nt@users.noreply.github.com>
2024-03-20 17:11:37 +0000

    Create DefaultT2CareConfiguration

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.DefaultCommitConfiguration = exports.DefaultConfiguration = void 0;
+exports.DefaultT2CareConfiguration = exports.DefaultCommitConfiguration = exports.DefaultConfiguration = void 0;
 exports.DefaultConfiguration = {
     max_tags_to_fetch: 200, // the amount of tags to fetch from the github API
     max_pull_requests: 200, // the amount of pull requests to process
@@ -93,6 +93,65 @@ exports.DefaultCommitConfiguration = {
     custom_placeholders: exports.DefaultConfiguration.custom_placeholders,
     trim_values: exports.DefaultConfiguration.trim_values
 };
+exports.DefaultT2CareConfiguration = {
+    max_tags_to_fetch: 200, // the amount of tags to fetch from the github API
+    max_pull_requests: 200, // the amount of pull requests to process
+    max_back_track_time_days: 365, // allow max of 365 days back to check up on pull requests
+    exclude_merge_branches: [], // branches to exclude from counting as PRs (e.g. YourOrg/qa, YourOrg/main)
+    sort: {
+        // defines the sorting logic for PRs
+        order: 'ASC', // the sorting order
+        on_property: 'mergedAt' // the property to sort on. (mergedAt falls back to createdAt)
+    },
+    template: '#{{CHANGELOG}}', // the global template to host the changelog
+    pr_template: '- #{{TITLE}} ##{{NUMBER}}', // the per PR template to pick
+    empty_template: '- no changes', // the template to use if no pull requests are found
+    categories: [
+        {
+            title: '## 🚀 Features',
+            labels: ['feature', 'enhancement 🤪🦄']
+        },
+        {
+            title: '## 🐛 Fixes',
+            labels: ['fix', 'bug 🕵️‍♂️']
+        },
+        {
+            title: '## 🧪 Tests',
+            labels: ['test', 'test 🧪🤞']
+        },
+        {
+            title: '## 📚 Docs',
+            labels: ['📚 Docs']
+        },
+        {
+            title: '## 📚 UI',
+            labels: ['ui 👀']
+        },
+        {
+            title: '## 📦 Uncategorized',
+            labels: []
+        }
+    ], // the categories to support for the ordering
+    ignore_labels: ['ignore', 'skip-changelog'], // list of lables being ignored from the changelog
+    label_extractor: [
+        {
+            pattern: '(\\w+) (.+)',
+            target: '$1',
+            on_property: 'title'
+        }
+    ], // extracts additional labels from the commit message given a regex
+    duplicate_filter: undefined, // extract an identifier from a PR used to detect duplicates, will keep the last match (depends on `sort`)
+    transformers: [], // transformers to apply on the PR description according to the `pr_template`
+    tag_resolver: {
+        // defines the logic on how to resolve the previous tag, only relevant if `fromTag` is not specified
+        method: 'semver', // defines which method to use, by default it will use `semver` (dropping all non matching tags). Alternative `sort` is also available.
+        filter: undefined, // filter out all tags not matching the regex
+        transformer: undefined // transforms the tag name using the regex, run after the filter
+    },
+    base_branches: [], // target branches for the merged PR ignoring PRs with different target branch, by default it will get all PRs
+    custom_placeholders: [],
+    trim_values: false // defines if values are being trimmed prior to inserting
+};
 
 
 /***/ }),
@@ -139,6 +198,7 @@ const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const utils_1 = __nccwpck_require__(918);
 const releaseNotesBuilder_1 = __nccwpck_require__(4883);
+const configuration_1 = __nccwpck_require__(5527);
 const GithubRepository_1 = __nccwpck_require__(5887);
 const GiteaRepository_1 = __nccwpck_require__(4349);
 function run() {
@@ -174,13 +234,13 @@ function run() {
             }
             // read in the configuration from the file if possible
             const configurationFile = core.getInput('configuration');
-            let configFile = (0, utils_1.resolveConfiguration)(repositoryPath, configurationFile);
+            const configFile = (0, utils_1.resolveConfiguration)(repositoryPath, configurationFile);
             if (configFile) {
                 core.info(`ℹ️ Retrieved configuration via 'configuration' (via file).`);
             }
             if (!configJson && !configFile) {
-                core.info(`ℹ️ No configuration provided. Using configuration_t2care.json.`);
-                configFile = (0, utils_1.resolveConfiguration)('', 'configs/configuration_t2care.json');
+                core.info(`ℹ️ No configuration provided. Using DefaultT2CareConfiguration.`);
+                configJson = configuration_1.DefaultT2CareConfiguration;
             }
             // mode of the action (PR, COMMIT, HYBRID)
             const mode = (0, utils_1.resolveMode)(core.getInput('mode'), core.getInput('commitMode') === 'true');
--- src/configuration.ts
+++ src/configuration.ts
@@ -149,3 +149,63 @@ export const DefaultCommitConfiguration: Configuration = {
   custom_placeholders: DefaultConfiguration.custom_placeholders,
   trim_values: DefaultConfiguration.trim_values
 }
+
+export const DefaultT2CareConfiguration: Configuration = {
+  max_tags_to_fetch: 200, // the amount of tags to fetch from the github API
+  max_pull_requests: 200, // the amount of pull requests to process
+  max_back_track_time_days: 365, // allow max of 365 days back to check up on pull requests
+  exclude_merge_branches: [], // branches to exclude from counting as PRs (e.g. YourOrg/qa, YourOrg/main)
+  sort: {
+    // defines the sorting logic for PRs
+    order: 'ASC', // the sorting order
+    on_property: 'mergedAt' // the property to sort on. (mergedAt falls back to createdAt)
+  },
+  template: '#{{CHANGELOG}}', // the global template to host the changelog
+  pr_template: '- #{{TITLE}} ##{{NUMBER}}', // the per PR template to pick
+  empty_template: '- no changes', // the template to use if no pull requests are found
+  categories: [
+    {
+      title: '## 🚀 Features',
+      labels: ['feature', 'enhancement 🤪🦄']
+    },
+    {
+      title: '## 🐛 Fixes',
+      labels: ['fix', 'bug 🕵️‍♂️']
+    },
+    {
+      title: '## 🧪 Tests',
+      labels: ['test', 'test 🧪🤞']
+    },
+    {
+      title: '## 📚 Docs',
+      labels: ['📚 Docs']
+    },
+    {
+      title: '## 📚 UI',
+      labels: ['ui 👀']
+    },
+    {
+      title: '## 📦 Uncategorized',
+      labels: []
+    }
+  ], // the categories to support for the ordering
+  ignore_labels: ['ignore', 'skip-changelog'], // list of lables being ignored from the changelog
+  label_extractor: [
+    {
+      pattern: '(\\w+) (.+)',
+      target: '$1',
+      on_property: 'title'
+    }
+  ], // extracts additional labels from the commit message given a regex
+  duplicate_filter: undefined, // extract an identifier from a PR used to detect duplicates, will keep the last match (depends on `sort`)
+  transformers: [], // transformers to apply on the PR description according to the `pr_template`
+  tag_resolver: {
+    // defines the logic on how to resolve the previous tag, only relevant if `fromTag` is not specified
+    method: 'semver', // defines which method to use, by default it will use `semver` (dropping all non matching tags). Alternative `sort` is also available.
+    filter: undefined, // filter out all tags not matching the regex
+    transformer: undefined // transforms the tag name using the regex, run after the filter
+  },
+  base_branches: [], // target branches for the merged PR ignoring PRs with different target branch, by default it will get all PRs
+  custom_placeholders: [],
+  trim_values: false // defines if values are being trimmed prior to inserting
+}
